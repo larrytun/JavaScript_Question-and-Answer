ddd1f6c378912fb445111e2e16923f16f9d0da41
Larry Tunseth <larrydeantun@gmail.com>
2016-10-28 10:08:51 -0700

    add add question functionality and fix delete functionality

---
+++ app/components/new-question.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        date: this.get('date'),
+        content: this.get('content'),
+        avatar: this.get('avatar'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
+});
