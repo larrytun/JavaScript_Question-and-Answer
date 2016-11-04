922ebe05b668d331c0125a3c7d0ae7e8a4996990
Larry Tunseth <larrydeantun@gmail.com>
2016-11-04 12:21:38 -0700

    add custom helper to assign an icon to popular posts

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -10,6 +10,6 @@ export default Ember.Component.extend({
     },
     update(answer, params) {
       this.sendAction('update', answer, params);
-    }
+    },
   }
 });
---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function questionPopularity(params) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') >= 5) {
+    return Ember.String.htmlSafe('<img src="https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/leisa_christmas_false_color.png?itok=Jxf0IlS4" height="50" width="50">');
+  }
+}
+
+export default Ember.Helper.helper(questionPopularity);
