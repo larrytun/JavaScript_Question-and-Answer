a102fa81429eb696a43b1c0f3bfa611b7a9d283a
Larry Tunseth <larrydeantun@gmail.com>
2016-11-04 10:55:16 -0700

    add helper to display current time and time on answers

---
+++ tests/unit/helpers/ember-moment-test.js
@@ -0,0 +1,12 @@
+
+import { emberMoment } from 'question-and-answer/helpers/ember-moment';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | ember moment');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = emberMoment([42]);
+  assert.ok(result);
+});
+
